e3294ac37cfa4e2ea310b5ac6d6b80a2c6ef36c8
Simona Zozuľaková <szozulakova@gmail.com>
2022-11-28 18:47:44 +0100

    moking fetch for songs

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const path = require("path");
 const port = 3000;
 app.use(express.static("public"));
 //---------------------------------------------------
-/*let mysql = require("mysql");
+const mysql = require("mysql");
 let conn = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
-  database: "foxPlayer",
+  database: "music_player",
 });
 conn.connect((err) => {
   if (err) {
@@ -19,12 +19,33 @@ conn.connect((err) => {
     return;
   }
   console.log("Connection established");
-});*/
+});
 //---------------------------------------------------
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/player.html"));
 });
+
+/*app.get("/playlist-tracks/:playlist_id", (req, res) => {
+  const playlist_id = req.params.playlist_id;
+  const tracksPromise = new Promise((res, rej) => {
+    conn.query(
+      `SELECT * FROM tracks WHERE playlist_id=${playlist_id}`,
+      (err, rows) => {
+        if (err) {
+          rej({ error: "Database error" });
+        }
+        res(rows);
+      }
+    );
+  });
+
+  const apiHandler = async () => {
+    const response = await tracksPromise.then((res) => res).catch((err) => err);
+    res.send(response);
+  };
+  apiHandler();
+});*/
 
 app.listen(port, () => {
   console.log(`The server is up and running on ${port}`);
--- public/player.js
+++ public/player.js
@@ -1,5 +1,70 @@
 "use strict";
+const fakeFetch = () => {
+  const fakePromise = new Promise((res, rej) => {
+    res([
+      {
+        id: 1,
+        playlist_id: 2,
+        title: "Spirirt Blossom",
+        artist: "RomanBelov",
+        duration: 1.48,
+        path: "public/music/spirit-blossom-15285.mp3",
+      },
+      {
+        id: 2,
+        playlist_id: 2,
+        title: "WatR-Fluid",
+        artist: "ItsWatR",
+        duration: 2.13,
+        path: "public/music/watr-fluid-10149.mp3",
+      },
+      {
+        id: 3,
+        playlist_id: 2,
+        title: "Chillmore",
+        artist: "The Weekend",
+        duration: 2.25,
+        path: "public/music/he-weekend-117427.mp3",
+      },
+      {
+        id: 4,
+        playlist_id: 2,
+        title: "Sunset Vibes",
+        artist: "Undefined",
+        duration: 2.28,
+        path: "public/music/sunset-vibes-lo-fichillhop-9503.mp3",
+      },
+    ]);
+    rej("Database error");
+  });
+  return fakePromise;
+};
 
+//CREATING HTML FOR DONGS LIST
+const songsWrapper = document.querySelector(".songsWrapper");
+
+const createLi = (title) => {
+  const song = document.createElement("a");
+  song.innerText = title;
+  song.className = "song";
+  songsWrapper.appendChild(song);
+};
+const getSongsHandler = async () => {
+  const songs = await fakeFetch()
+    .then((res) => res)
+    .catch((err) => err);
+
+  songs.forEach((song) => {
+    createLi(song.title);
+  });
+  songs.forEach((song) => {
+    createLi(song.title);
+  });
+  songs.forEach((song) => {
+    createLi(song.title);
+  });
+};
+getSongsHandler();
 //loadstart, play, ended, progress
 const playButton = document.getElementById("play");
 const media = document.querySelector("audio");
@@ -21,6 +86,9 @@ media.addEventListener("ended", (e) => {
 
 media.addEventListener("loadstart", (e) => {
   console.log("loadstart");
+});
+media.addEventListener("progress", (e) => {
+  console.log("progress");
 });
 
 media.addEventListener("play", (e) => {
